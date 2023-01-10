117979ea581ec01054d3ff74803778e86e7d22bb
Manoel Rodrigo Cândido <rodrigocan@gmail.com>
2023-01-10 19:08:36 -0300

    Componente: Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { ThemeProvider } from 'styled-components'
+import { Transactions } from './pages/Transactions'
 import { defaultTheme } from './styles//themes/default'
 import { GlobalStyle } from './styles/global'
 
@@ -7,7 +8,7 @@ export function App() {
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
 
-      <h1>Hello World</h1>
+      <Transactions />
     </ThemeProvider>
   )
 }
